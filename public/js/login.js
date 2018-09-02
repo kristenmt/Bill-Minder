98cb04fc87019149229427383f09a893d62eabfa
Jennifer Cobb <jenn.cobb.m@gmail.com>
2018-09-02 19:57:56 -0400

    redirect to dashboard on log in

--- public/js/login.js
+++ public/js/login.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
   // Getting references to our form and inputs
   var loginForm = $("form.login");
-  var emailInput = $("input#email-input");
-  var passwordInput = $("input#password-input");
+  var emailInput = $("input#email-input-login");
+  var passwordInput = $("input#password-input-login");
   
 
   // When the form is submitted, we validate there's an email and password entered
   loginForm.on("submit", function(event) {
     event.preventDefault();
+    alert("Login requested");
     var userData = {
       email: emailInput.val().trim(),
       password: passwordInput.val().trim()
